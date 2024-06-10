9c31714ac79c9b7c57cb9d3ee6fae285c4de440b
Tiruu <lino.thebault@gmail.com>
2024-06-10 14:53:11 +0200

    added copy of index game

---
+++ siteWeb - Copie/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "UDSP89-Point_Click-0.1.1";
+const contentToCache = [
+    "Build/siteWeb - Copie.loader.js",
+    "Build/siteWeb - Copie.framework.js.br",
+    "Build/siteWeb - Copie.data.br",
+    "Build/siteWeb - Copie.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
